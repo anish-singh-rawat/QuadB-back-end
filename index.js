a4575f29385f6b4813a5df9157d8b0bfc7a0ece8
anish-singh-rawat <anishsinghrawat1>
2024-06-06 10:18:39 +0530

    write some code for resolved CROSS origin error

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+// import bodyParser from 'body-parser';
+// import express from 'express';
+// import cors from "cors";
+// import dotenv from 'dotenv'
+
+// import { connectToMongoDB } from './config.js';
+// import router from './routes/AuthRoutes.js';
+// import ProductRouter from './routes/ProductRoutes.js';
+// import CartRoute from './routes/CartRoute.js';
+// import OrderRoute from './routes/OrderRoute.js';
+
+
+// dotenv.config()
+// const app = express()
+// app.use(bodyParser.json())
+// app.use(cors());
+// app.use(express.json())
+// app.use('/public', express.static('public'));
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// connectToMongoDB();
+
+// app.get('/', (req, res) => {
+//     res.status(200).send({ message: "Welcome to QuadB tech Group API's" });
+// });
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`E-commerce backend listening at http://localhost:${process.env.PORT}`)
+// })
+
+// app.use('/auth', router);
+// app.use('/product', ProductRouter);
+// app.use('/cart', CartRoute);
+// app.use('/order', OrderRoute);
+
+
 import bodyParser from 'body-parser';
 import express from 'express';
-import cors from "cors";
-import dotenv from 'dotenv'
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import { connectToMongoDB } from './config.js';
 import router from './routes/AuthRoutes.js';
@@ -9,24 +45,26 @@ import ProductRouter from './routes/ProductRoutes.js';
 import CartRoute from './routes/CartRoute.js';
 import OrderRoute from './routes/OrderRoute.js';
 
+dotenv.config();
 
-dotenv.config()
-const app = express()
-app.use(bodyParser.json())
-app.use(cors());
-app.use(express.json())
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json());
 app.use('/public', express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
+
 connectToMongoDB();
 
 app.get('/', (req, res) => {
     res.status(200).send({ message: "Welcome to QuadB tech Group API's" });
 });
-
-app.listen(process.env.PORT, () => {
-    console.log(`E-commerce backend listening at http://localhost:${process.env.PORT}`)
-})
 
 app.use('/auth', router);
 app.use('/product', ProductRouter);
@@ -34,3 +72,7 @@ app.use('/cart', CartRoute);
 app.use('/order', OrderRoute);
 
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`E-commerce backend listening at http://localhost:${PORT}`);
+});
